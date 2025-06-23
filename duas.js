f4c4fb66ab90123bc5aac8b93057ad3d3da9df51
Alito24 <147553250+Alito24@users.noreply.github.com>
2025-06-23 10:45:58 +0200

    Create duas.js

---
+++ duas.js
@@ -0,0 +1,10 @@
+const duas = [
+    // Marriage Section
+    {
+        section: 'marriage',
+        title: 'Dua for a righteous spouse',
+        text: 'Our Lord, grant us from among our wives and offspring comfort to our eyes and make us an example for the righteous.',
+        atext: 'رَبَّنَا هَبْ لَنَا مِنْ أَزْوَاجِنَا وَذُرِّيَّاتِنَا قُرَّةَ أَعْيُنٍ وَاجْعَلْنَا لِلْمُتَّقِينَ إِمَامًا',
+        book: 'Quran 25:74'
+    }
+  ];
