306d28f89d70b78abe9a1797b160ab7302ff2b3f
Alito24 <147553250+Alito24@users.noreply.github.com>
2025-06-23 10:45:04 +0200

    Create sections.js

---
+++ sections.js
@@ -0,0 +1,18 @@
+const sections = [
+    {
+        id: 'protection',
+        name: 'Protection'
+    },
+    {
+        id: 'forgiveness',
+        name: 'Forgiveness'
+    },
+    {
+        id: 'health',
+        name: 'Health & Healing'
+    },
+    {
+        id: 'marriage',
+        name: 'Marriage'
+    },
+];
