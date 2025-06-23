57cf4ea7f2b0e489be3901cedd201616b51441bb
Alito24 <147553250+Alito24@users.noreply.github.com>
2025-06-23 16:18:38 +0200

    Update duas.js

--- duas.js
+++ duas.js
@@ -8,14 +8,14 @@ const duas = [
     },
     {
         section: 'marriage',
-        title: 'Dua for a righteous spouse',
+        title: 'Righteous spouse and children',
         text: 'Our Lord, grant us from among our wives and offspring comfort to our eyes and make us an example for the righteous.',
         atext: 'رَبَّنَا هَبْ لَنَا مِنْ أَزْوَاجِنَا وَذُرِّيَّاتِنَا قُرَّةَ أَعْيُنٍ وَاجْعَلْنَا لِلْمُتَّقِينَ إِمَامًا',
         book: 'Quran 25:74'
     },
     {
         section: 'marriage',
-        title: 'Dua for a newly-wed couple',
+        title: 'For a newly-wed couple',
         text: 'May Allah grant you blessings, send blessings upon you, and bring you together in goodness.',
         atext: 'بَارَكَ اللَّهُ لَكَ وَبَارَكَ عَلَيْكَ وَجَمَعَ بَيْنَكُمَا فِي الْخَيْرِ',
         book: 'Sunan al-Tirmidhī 1091'
